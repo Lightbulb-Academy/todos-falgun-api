5151c6830cc3fe382beab54d006c06caf38992b7
Shashank <shashankj677@gmail.com>
2025-03-28 14:56:23 +0545

    implemented auth guard

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}

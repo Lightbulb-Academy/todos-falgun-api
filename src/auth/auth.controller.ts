a55dffe55111358bd223bd673542a383551058a2
Shashank <shashankj677@gmail.com>
2025-04-09 13:50:05 +0545

    added profile api get and update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, Patch } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { Public } from 'src/helpers/public';
+import { Payload } from 'src/interfaces/payload';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,18 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Get('profile')
+  profile(@Req() req: Payload) {
+    return this.authService.profile(req.payload.id);
+  }
+
+  @Patch('profile')
+  updateProfile(
+    @Req() req: Payload,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    return this.authService.updateProfile(req.payload.id, updateProfileDto);
   }
 }
